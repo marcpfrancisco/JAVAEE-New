8a9beb7c66a6e1ebd87e56653d03edbcd8a03bee
Marc Francisco <marcpfrancisco@gmail.com>
2018-11-06 12:16:10 +0800

    commit suicide

--- pages/passing-chance/js/index.js
+++ pages/passing-chance/js/index.js
@@ -3,12 +3,7 @@
   var chat = {
     messageToSend: '',
     messageResponses: [
-      'Why did the web developer leave the restaurant? Because of the table layout.',
-      'How do you comfort a JavaScript bug? You console it.',
-      'An SQL query enters a bar, approaches two tables and asks: "May I join you?"',
-      'What is the most used language in programming? Profanity.',
-      'What is the object-oriented way to become wealthy? Inheritance.',
-      'An SEO expert walks into a bar, bars, pub, tavern, public house, Irish pub, drinks, beer, alcohol'
+      'YES', 'NO', 'MAYBE', 'SOMETIMES', 'NEVER'
     ],
     init: function() {
       this.cacheDOM();
@@ -78,23 +73,5 @@
   };
   
   chat.init();
-  
-  var searchFilter = {
-    options: { valueNames: ['name'] },
-    init: function() {
-      var userList = new List('people-list', this.options);
-      var noItems = $('<li id="no-items-found">No items found</li>');
-      
-      userList.on('updated', function(list) {
-        if (list.matchingItems.length === 0) {
-          $(list.list).append(noItems);
-        } else {
-          noItems.detach();
-        }
-      });
-    }
-  };
-  
-  searchFilter.init();
-  
+ 
 })();
